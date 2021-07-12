ab965525995688c9c84f3e4f0d20c3b706efaa0e
mokweri <mogsobd@gmail.com>
2021-07-12 16:45:42 +0300

    started usart

---
+++ STM32f103xx_drivers/drivers/Inc/stm32f103xx_usart_driver.h
@@ -0,0 +1,17 @@
+/*
+ * stm32f103xx_usart_driver.h
+ *
+ *  Created on: 12 Jul 2021
+ *      Author: OBED
+ */
+
+#ifndef INC_STM32F103XX_USART_DRIVER_H_
+#define INC_STM32F103XX_USART_DRIVER_H_
+
+#include "stm32f103xx.h"
+
+
+
+
+
+#endif /* INC_STM32F103XX_USART_DRIVER_H_ */
---
+++ STM32f103xx_drivers/drivers/Src/stm32f103xx_usart.c
@@ -0,0 +1,9 @@
+/*
+ * stm32f103xx_usart.c
+ *
+ *  Created on: 12 Jul 2021
+ *      Author: OBED
+ */
+#include "stm32f103xx_usart_driver.h"
+
+
