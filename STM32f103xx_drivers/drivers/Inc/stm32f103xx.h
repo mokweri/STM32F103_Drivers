bff939c035ce8ea573df389ddcef9455f043ad23
mokweri <mogsobd@gmail.com>
2021-07-16 16:31:08 +0300

    started USART drivers

--- STM32f103xx_drivers/drivers/Inc/stm32f103xx.h
+++ STM32f103xx_drivers/drivers/Inc/stm32f103xx.h
@@ -76,6 +76,10 @@
 #define SPI3_BASEADDR					 (APB1PERIPH_BASEADDR + 0x3C00)
 #define I2C1_BASEADDR					 (APB1PERIPH_BASEADDR + 0x5400)
 #define I2C2_BASEADDR					 (APB1PERIPH_BASEADDR + 0x5800)
+#define USART2_BASEADDR					 (APB1PERIPH_BASEADDR + 0x4400)
+#define USART3_BASEADDR					 (APB1PERIPH_BASEADDR + 0x4800)
+#define UART4_BASEADDR					 (APB1PERIPH_BASEADDR + 0x4C00)
+#define UART5_BASEADDR					 (APB1PERIPH_BASEADDR + 0x5000)
 
 
 /*
@@ -93,7 +97,7 @@
 #define EXTI_BASEADDR					 (APB2PERIPH_BASEADDR + 0x0400)
 
 #define SPI1_BASEADDR					 (APB2PERIPH_BASEADDR + 0x3000)
-
+#define USART1_BASEADDR					 (APB2PERIPH_BASEADDR + 0x3800)
 /*
  * Base addresses of peripherals which are hanging on AHB bus
  * TODO : Complete for all other peripherals
@@ -208,6 +212,20 @@ typedef struct
   __vo uint32_t TRISE;      /*!< I2C TRISE register,     										Address offset: 0x20 */
 }I2C_RegDef_t;
 
+/*
+ * peripheral register definition structure for USART
+ */
+typedef struct
+{
+	__vo uint32_t SR;         /*!< Status register,     										Address offset: 0x00 */
+	__vo uint32_t DR;         /*!< Data register,     											Address offset: 0x04 */
+	__vo uint32_t BRR;        /*!< Baud rate register,     										Address offset: 0x08 */
+	__vo uint32_t CR1;        /*!< Control register 1,     										Address offset: 0x0C */
+	__vo uint32_t CR2;        /*!< Control register 2,     										Address offset: 0x10 */
+	__vo uint32_t CR3;        /*!< Control register 3,     										Address offset: 0x14 */
+	__vo uint32_t GTPR;       /*!< Guard time and prescaler register,     						Address offset: 0x18 */
+} USART_RegDef_t;
+
 
 
 /*
@@ -231,6 +249,12 @@ typedef struct
 
 #define I2C1				((I2C_RegDef_t*)I2C1_BASEADDR)
 #define I2C2				((I2C_RegDef_t*)I2C2_BASEADDR)
+
+#define USART1  			((USART_RegDef_t*)USART1_BASEADDR)
+#define USART2  			((USART_RegDef_t*)USART2_BASEADDR)
+#define USART3  			((USART_RegDef_t*)USART3_BASEADDR)
+#define UART4  				((USART_RegDef_t*)UART4_BASEADDR)
+#define UART5  				((USART_RegDef_t*)UART5_BASEADDR)
 
 
 
@@ -261,6 +285,23 @@ typedef struct
 #define SP1_PCLK_DI()		(RCC->APB2ENR &= ~(1 << 12))
 #define SP2_PCLK_DI()		(RCC->APB1ENR &= ~(1 << 14))
 #define SP3_PCLK_DI()		(RCC->APB1ENR &= ~(1 << 15))
+
+
+/*
+ * Clock Enable Macros for USARTx peripherals
+ */
+#define USART1_PCCK_EN() (RCC->APB2ENR |= (1 << 14))
+#define USART2_PCCK_EN() (RCC->APB1ENR |= (1 << 17))
+#define USART3_PCCK_EN() (RCC->APB1ENR |= (1 << 18))
+#define UART4_PCCK_EN()  (RCC->APB1ENR |= (1 << 19))
+#define UART5_PCCK_EN()  (RCC->APB1ENR |= (1 << 20))
+
+#define USART1_PCCK_DI() (RCC->APB2ENR &= ~(1 << 14))
+#define USART2_PCCK_DI() (RCC->APB1ENR &= ~(1 << 17))
+#define USART3_PCCK_DI() (RCC->APB1ENR &= ~(1 << 18))
+#define UART4_PCCK_DI()  (RCC->APB1ENR &= ~(1 << 19))
+#define UART5_PCCK_DI()  (RCC->APB1ENR &= ~(1 << 20))
+
 
 /*
  * Clock Enable Macros for I2Cx peripherals
@@ -318,7 +359,6 @@ typedef struct
 #define IRQ_NO_USART3	    39
 #define IRQ_NO_UART4	    52
 #define IRQ_NO_UART5	    53
-#define IRQ_NO_USART6	    71
 
 
 //some generic macros
@@ -448,12 +488,82 @@ typedef struct
 #define I2C_CCR_DUTY 					14
 #define I2C_CCR_FS  				 	15
 
+/******************************************************************************************
+ *Bit position definitions of USART peripheral
+ ******************************************************************************************/
+
+/*
+ * Bit position definitions USART_CR1
+ */
+#define USART_CR1_SBK					0
+#define USART_CR1_RWU 					1
+#define USART_CR1_RE  					2
+#define USART_CR1_TE 					3
+#define USART_CR1_IDLEIE 				4
+#define USART_CR1_RXNEIE  				5
+#define USART_CR1_TCIE					6
+#define USART_CR1_TXEIE					7
+#define USART_CR1_PEIE 					8
+#define USART_CR1_PS 					9
+#define USART_CR1_PCE 					10
+#define USART_CR1_WAKE  				11
+#define USART_CR1_M 					12
+#define USART_CR1_UE 					13
+#define USART_CR1_OVER8  				15
+
+
+
+/*
+ * Bit position definitions USART_CR2
+ */
+#define USART_CR2_ADD   				0
+#define USART_CR2_LBDL   				5
+#define USART_CR2_LBDIE  				6
+#define USART_CR2_LBCL   				8
+#define USART_CR2_CPHA   				9
+#define USART_CR2_CPOL   				10
+#define USAR _CR2_CLKEN					11
+#define USART_CR2_STOP   				12
+#define USART_CR2_LINEN   				14
+
+
+/*
+ * Bit position definitions USART_CR3
+ */
+#define USART_CR3_EIE   				0
+#define USART_CR3_IREN   				1
+#define USART_CR3_IRLP  				2
+#define USART_CR3_HDSEL   				3
+#define USART_CR3_NACK   				4
+#define USART_CR3_SCEN   				5
+#define USART_CR3_DMAR  				6
+#define USART_CR3_DMAT   				7
+#define USART_CR3_RTSE   				8
+#define USART_CR3_CTSE   				9
+#define USART_CR3_CTSIE   				10
+
+/*
+ * Bit position definitions USART_SR
+ */
+#define USART_SR_PE        				0
+#define USART_SR_FE        				1
+#define USART_SR_NE        				2
+#define USART_SR_ORE       				3
+#define USART_SR_IDLE       			4
+#define USART_SR_RXNE        			5
+#define USART_SR_TC        				6
+#define USART_SR_TXE        			7
+#define USART_SR_LBD        			8
+#define USART_SR_CTS        			9
+
+
 
 
 #include "stm32f103xx_rcc_driver.h"
 #include "stm32f103xx_gpio_driver.h"
 #include "stm32f103xx_spi_driver.h"
 #include "stm32f103xx_i2c_driver.h"
+#include "stm32f103xx_usart_driver.h"
 
 
 #endif /* INC_STM32F103XX_H_ */
--- STM32f103xx_drivers/drivers/Src/stm32f103xx_usart.c
+++ STM32f103xx_drivers/drivers/Src/stm32f103xx_usart.c
@@ -7,3 +7,50 @@
 #include "stm32f103xx_usart_driver.h"
 
 
+void USART_PeriClockControl(USART_RegDef_t *pUSARTx, uint8_t EnOrDi)
+{
+	if(EnorDi == ENABLE)
+	{
+		if(pUSARTx == USART1)
+		{
+			USART1_PCCK_EN();
+		}else if (pUSARTx == USART2)
+		{
+			USART2_PCCK_EN();
+		}else if (pUSARTx == USART3)
+		{
+			USART3_PCCK_EN();
+		}
+		else if (pUSARTx == UART4)
+		{
+			UART4_PCCK_EN();
+		}else if(pUSARTx == UART5)
+		{
+			UART5_PCCK_EN();
+		}
+	}
+	else
+	{
+		//TODO
+	}
+}
+
+/*********************************************************************
+ * @fn      		  - USART_Init
+ *
+ * @brief             -
+ *
+ * @param[in]         -
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -
+ *
+ * @Note              -
+
+ */
+void USART_Init(USART_Handle_t *pUSARTHandle)
+{
+
+}
+
